836906739d420439d37e2f6cedd6ae002a3f00bd
ConectaIC <conecta.ing@gmail.com>
2025-08-12 18:31:03 +0200

    Update supabaseAdmin.ts

--- src/lib/supabaseAdmin.ts
+++ src/lib/supabaseAdmin.ts
@@ -1,2 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-export const supabaseAdmin=()=>{const url=process.env.NEXT_PUBLIC_SUPABASE_URL as string;const key=process.env.SUPABASE_SERVICE_ROLE_KEY as string;return createClient(url,key,{auth:{persistSession:false}})};
+
+export const supabaseAdmin = () => {
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+  const key = process.env.SUPABASE_SERVICE_ROLE_KEY as string;
+  return createClient(url, key, { auth: { persistSession: false } });
+};
